c9a3b1f49c30f39b4211d39c4e98506f72b558a0
KonradB <55328364+KonraddB@users.noreply.github.com>
2020-06-06 14:49:35 +0200

    Excercise 1 and 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,41 @@
 /* Here goes your JS code */
+
+let modal = document.querySelector('.popup');
+let openModal = document.getElementById('show-popup-form');
+let closeModal = document.querySelector('.close-popup');
+let btnForm = document.querySelector('.btn-form');
+
+// openModal.onclick = () => {
+//     modalBox();
+// }
+
+// closeModal.onclick = () => {
+//     modalBox();
+// }
+
+// modal.onclick = (event) => {
+//     if(event.target == modal) {
+//         modalBox()
+//     }
+// }
+
+// const modalBox = () => {
+//     modal.classList.toggle('open');
+
+openModal.onclick=function(){
+    modalBox();
+}
+closeModal.onclick=function(){
+    modalBox();
+}
+// when user clicks anywhere outside of the modal , close it
+modal.onclick=function(event){
+   if(event.target==modal){
+     modalBox();        
+   }
+}
+
+function modalBox(){
+    modal.classList.toggle("open");
+}
+ 

34c5e1be7ff13b550c315f568a1dd5587cad30eb
KonradB <55328364+KonraddB@users.noreply.github.com>
2020-06-07 14:59:53 +0200

    Exercise 3

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,60 @@
 /* Here goes your JS code */
 
-let modal = document.querySelector('.popup');
-let openModal = document.getElementById('show-popup-form');
-let closeModal = document.querySelector('.close-popup');
-let btnForm = document.querySelector('.btn-form');
+const modal = document.querySelector(".popup");
+const openModal = document.getElementById("show-popup-form");
+const closeModal = document.querySelector(".close-popup");
+const btnForm = document.querySelector(".btn-form");
+const successMess = document.getElementById("show-success-mess");
 
-// openModal.onclick = () => {
-//     modalBox();
-// }
+openModal.onclick = () => {
+  modalBox();
+};
 
-// closeModal.onclick = () => {
-//     modalBox();
-// }
+closeModal.onclick = () => {
+  modalBox();
+};
 
-// modal.onclick = (event) => {
-//     if(event.target == modal) {
-//         modalBox()
-//     }
-// }
-
-// const modalBox = () => {
-//     modal.classList.toggle('open');
-
-openModal.onclick=function(){
+modal.onclick = (event) => {
+  if (event.target == modal) {
     modalBox();
-}
-closeModal.onclick=function(){
-    modalBox();
-}
-// when user clicks anywhere outside of the modal , close it
-modal.onclick=function(event){
-   if(event.target==modal){
-     modalBox();        
+  }
+};
+
+const modalBox = () => {
+  modal.classList.toggle("open");
+};
+
+btnForm.onclick = () => {
+   if(validate() === false ){
+    validate();
+   } else {
+    setInterval(function sumbitForm() {
+        modal.classList.remove("open");
+      }, 3000);
+    
+      openModal.classList.add("hide");
+      successMess.classList.remove("hide");
    }
-}
 
-function modalBox(){
-    modal.classList.toggle("open");
+};
+
+const validate = () => {
+    const passInput = document.querySelector('.password').value,
+        mailInput = document.querySelector('.email').value,
+        errorInput = document.querySelector('.error-message');
+
+    let text;
+
+    if(mailInput.length < 8 || mailInput.indexOf('@') == -1) {
+        text= "Please enter valid e-mail";
+        errorInput.innerHTML = text;
+        return false;
+    }
+
+    if(passInput.length < 8) {
+        text= "Please enter valid password";
+        errorInput.innerHTML = text;
+        return false;
+    }
+    return true;
 }
- 
